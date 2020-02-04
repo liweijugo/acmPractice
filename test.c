9da40add8a9ab9e8b6a9cb3a4f6f378b58f4054b
liweijugo <liweijugo@126.com>
2020-02-04 15:38:59 +0800

    first commit

---
+++ test.c
@@ -0,0 +1,18 @@
+//
+//  test1.c
+//  CtestProject
+//
+//  Created by Weiju Li on 2020/1/27.
+//  Copyright © 2020 Weiju Li. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("input a b:\n");
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a+b);
+    
+    return 0;
+}
