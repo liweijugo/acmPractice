77923e6132b7649e01e6467469005fc595de706a
liweijugo <liweijugo@126.com>
2020-02-04 18:15:53 +0800

    commit xcode project

---
+++ acmDemo/acmDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  acmDemo
+//
+//  Created by Weiju Li on 2020/2/4.
+//  Copyright © 2020 Weiju Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
